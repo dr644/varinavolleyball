7fdd98c500725c5efd82cf49ba9b6d5697502856
Dave <dr644@proton.me>
2025-11-03 17:59:26 -0500

    roster update

--- varinavolleyball/settings.py
+++ varinavolleyball/settings.py
@@ -23,17 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # UNCOMMENT WHEN UPLOADING
-# load_dotenv()
-# SECRET_KEY = os.getenv('SECRET_KEY')
-# DEBUG = os.getenv('DEBUG', 'False') == 'True'
-#
-# ALLOWED_HOSTS = ['varinavolleyball.com', 'www.varinavolleyball.com', '127.0.0.1']
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY')
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
+ALLOWED_HOSTS = ['varinavolleyball.com', 'www.varinavolleyball.com', '127.0.0.1']
 
-#REMOVE THIS WHEN MOVING TO PRODUCTION
-SECRET_KEY='django-insecure-ww1#f5y8@%!m=v-nytgp+6ngyaeg9acc3ev0$fw56g#=u6d!gp'
-DEBUG = True
-ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
